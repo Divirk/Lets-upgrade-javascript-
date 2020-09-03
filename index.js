c19e5d2109c0d4e8a3d475465e438b609cfaaa3b
Divirk <70430977+Divirk@users.noreply.github.com>
2020-09-03 15:05:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+//Program to find particular character in a string.
+let str= "letsfightback";
+let n=str.indexOf("h");
+console.log(n);
+
+//Program to convert minutes to seconds
+let min=15;
+seconds = min*60;
+console.log( " 15 " + " minutes " +" into seconds is "+ seconds);
+
+//Program to search element in a array of strings
+var array= [10,20,30,40,50];
+var result=array.find(function(number){
+return number>40
+});
+console.log(result);
+
+//Print an array in reverse order
+var array=["apple","ballon","chips","donkey","escape","fired"];
+array.reverse();
+console.log(array);
+
+//Print to display only elements containing 'a' in term a array
+var elements=["apple","ballon","chips","donkey","escape","fired"];
+console.log(elements);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
